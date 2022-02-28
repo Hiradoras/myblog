feabcd9d366617284f8122584ca363900c604dba
Hiradoras <forwoermind@gmail.com>
2022-02-28 18:18:35 +0600

    added post.py

--- theblog/views.py
+++ theblog/views.py
@@ -3,6 +3,7 @@ from re import template
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from .forms import PostForm
 
 class HomeView(ListView):
     model = Post # this Post is the object that we can use in html. 
@@ -16,6 +17,7 @@ class ArticleDetailView(DetailView):
 
 class AddPostView(CreateView):
     model = Post
+    form_class = PostForm
     template_name = 'add_post.html'
-    fields = '__all__'
-    #fields = ('title', 'body')
+    # fields = '__all__'
+    # fields = ('title', 'body') # We don't need to declare fields to use because form.py do this.
