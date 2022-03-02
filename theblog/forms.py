aca6485816e58d380eddc93bfd843edc372fe4c4
Hiradoras <forwoermind@gmail.com>
2022-03-02 17:37:05 +0600

    categories added

--- theblog/forms.py
+++ theblog/forms.py
@@ -1,18 +1,27 @@
-from dataclasses import field
-from logging import PlaceHolder
+from random import choice
 from django import forms
-from .models import Post
+from .models import Post, Category
+
+# You can hardcode it like that but it's not dynamic
+# choices = [('coding', 'coding'), ("sports","sports"), ('entertainment','entertainment')]
+
+# This is better choice. It's get categories directly from database.
+choices = Category.objects.all().values_list('name','name')
+
+choice_list = [x for x in choices]
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag','author','body')
+        fields = ('title', 'title_tag','author','body', 'category')
 
         widgets = {
             'title' : forms.TextInput(attrs={'class' : 'form-control'}),
             'title_tag' : forms.TextInput(attrs={'class' : 'form-control'}),
             'author' : forms.Select(attrs={'class' : 'form-control'}),
             'body' : forms.Textarea(attrs={'class' : 'form-control'}),
+            'category' : forms.Select(choices = choice_list,  attrs={'class' : 'form-control'}),
+
         }
 
 class EditForm(forms.ModelForm):
--- theblog/models.py
+++ theblog/models.py
@@ -1,7 +1,21 @@
+from unicodedata import category
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('home')
+
+
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -9,7 +23,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     post_date = models.DateField(auto_now_add=True)
-
+    category = models.CharField(max_length=255, default="uncategorized")
 
     def __str__(self) -> str:
         return self.title + " | " + str(self.author)
