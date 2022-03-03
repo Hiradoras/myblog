0cf589658b33bfb62d3d07cdf0f315f8621633dd
Hiradoras <forwoermind@gmail.com>
2022-03-03 10:23:21 +0600

    Categories dropdown and category list html added

--- theblog/urls.py
+++ theblog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from theblog.models import Category
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, CategoryListView
 
 urlpatterns = [
     path('', HomeView.as_view(), name = "home"),
@@ -10,7 +10,8 @@ urlpatterns = [
     path('add_category/', AddCategoryView.as_view(), name='add_category'),
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name = "update-post"),
     path('article/<int:pk>/remove', DeletePostView.as_view(), name = "delete-post"),
-    path('category/<str:cats>/', CategoryView, name='category')
+    path('category/<str:cats>/', CategoryView, name='category'),
+    path('category-list/', CategoryListView, name='category-list'),
 
 ]
  
--- theblog/views.py
+++ theblog/views.py
@@ -1,4 +1,6 @@
+from ast import arg
 from dataclasses import field, fields
+from multiprocessing import context
 from re import template
 from unicodedata import category
 from django.shortcuts import render
@@ -13,6 +15,16 @@ class HomeView(ListView):
     ordering = ['-post_date']
     # ordering = ['-id']
 
+    def get_context_data(self,*args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context['cat_menu'] = cat_menu
+        return context
+
+def CategoryListView(request):
+    cat_menu_list = Category.objects.all()
+    return render(request, 'category_list.html',{'cat_menu_list':cat_menu_list})
+
 def CategoryView(request, cats):
     category_posts = Post.objects.filter(category=cats.replace('-',' '))
     return render(request, 'categories.html',{'cats':cats.title().replace('-',' '), "category_posts":category_posts})
@@ -21,6 +33,12 @@ def CategoryView(request, cats):
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_detail.html'
+
+    def get_context_data(self,*args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(ArticleDetailView, self).get_context_data(*args, **kwargs)
+        context['cat_menu'] = cat_menu
+        return context
 
 
 class AddPostView(CreateView):
